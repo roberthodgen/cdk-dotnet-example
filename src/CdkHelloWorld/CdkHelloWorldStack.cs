f1268e9c86afc550163a46733eac348d25e6c5d3
Robert Hodgen <roberthodgen@gmail.com>
2019-11-09 10:30:32 -0500

    Initial commit

---
+++ src/CdkHelloWorld/CdkHelloWorldStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkHelloWorld
+{
+    public class CdkHelloWorldStack : Stack
+    {
+        public CdkHelloWorldStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/CdkHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            new CdkHelloWorldStack(app, "CdkHelloWorldStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
